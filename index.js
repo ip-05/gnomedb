303e894af1e2ff95ab8cc30f8b7f3401160cdf54
Yevgen <yyakovliev02@gmail.com>
2021-04-22 17:50:58 +0300

    binary search tree and data store

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-require('./lib/db');
+const DataStore = require('./lib/store.js');
+
+const db = new DataStore();
+
+db.insert({ name: 'Jenya', age: 18 });
+
+const me1 = db.select({ name: 'Jenya' });
+
+console.log(me1);
