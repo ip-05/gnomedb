4544c7c7ed080b25eaf00a41a2d815f6b3f050fb
Yevgen <yyakovliev02@gmail.com>
2021-04-23 11:04:13 +0300

    improved bst add and compare keys

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const BinarySearchTree = require('./lib/bst.js');
 
 const tree = new BinarySearchTree({ fieldKey: 'id' });
 
+tree.add({ id: -75, name: 'Yevgen' });
+tree.add({ id: -25, name: 'Yevgen' });
+tree.add({ id: -50, name: 'Yevgen' });
 tree.add({ id: 0, name: 'Yevgen' });
-tree.add({ id: 1, name: 'Jenya' });
-tree.add({ id: -55, name: 'Jenya' });
-tree.add({ id: 9, name: 'Jenya' });
-tree.add({ id: 5, name: 'Jenya' });
-tree.add({ id: 25, name: 'Jenya' });
+tree.add({ id: 50, name: 'Yevgen' });
+tree.add({ id: 75, name: 'Yevgen' });
+tree.add({ id: 100, name: 'Yevgen' });
 
-console.dir({ tree }, { depth: 100 });
+const treeMin = tree.getMin();
+const treeMax = tree.getMax();
+
+console.dir({ treeMin });
+console.dir({ treeMax });
