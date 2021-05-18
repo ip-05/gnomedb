8db990094db883131722b1c644c4e3e74389bb31
= <yyakovliev02@gmail.com>
2021-05-18 20:44:35 +0300

    fixed some stuff up

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ db.index('age');
 db.index('city');
 db.index('name');
 
+const luke = db.add({ name: 'Luke', age: 17, city: 'Amsterdam' });
 const bob = db.add({ name: 'Bob', age: 18, city: 'Paris' });
 const joe = db.add({ name: 'Joe', age: 19, city: 'Warsaw' });
 const mary = db.add({ name: 'Mary', age: 20, city: 'Warsaw' });
@@ -15,7 +16,6 @@ const dan = db.add({ name: 'Dan', age: 20, city: 'London' });
 const greaterThanEqualTo18 = db.find({ age: { $gte: 18 } });
 const greaterThan18 = db.find({ age: { $gt: 18 } });
 const warsaw = db.find({ city: 'Warsaw' });
+const lessThanEqualTo18 = db.find({ age: { $lte: 18 } });
 
-console.log(greaterThanEqualTo18);
-console.log(greaterThan18);
-console.log(warsaw);
+console.log(db.indeces.get('age'));
