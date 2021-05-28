dfa47339ee443635f2edfb663d7b5a7adc219069
Nikolai Chub <nikolaichub.me@gmail.com>
2021-05-28 03:00:25 +0300

    testing very basic rotation

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
 const BinarySearchTree = require('./lib/bst.js');
 
-const tree = new BinarySearchTree({ fieldKey: 'city' });
+const tree = new BinarySearchTree({ fieldKey: 'num' });
 
-// tree.add({ _id: 0, name: 'Jenya', city: 'Kyiv' });
-// tree.add({ _id: 1, name: 'Jenya2', city: 'Kyiv' });
-// tree.add({ _id: 2, name: 'Jenya3', city: 'Kyiv' });
-// tree.add({ _id: 3, name: 'Jenya4', city: 'Lviv' });
+// const node40 = tree.add({ num: 40 });
+// const node20 = tree.add({ num: 20 });
+// const node10 = tree.add({ num: 10 });
+// const node25 = tree.add({ num: 25 });
+// const node30 = tree.add({ num: 30 });
+// const node22 = tree.add({ num: 22 });
+// const node50 = tree.add({ num: 50 });
 
-const users = {
-  default: { _id: 0, name: 'default', city: 0 },
-  // Jenya : { _id: 1, name: 'Jenya', city: 12 },
-  Alina: { _id: 2, name: 'Alina', city: -2 },
-  // Artem : { _id: 3, name: 'Artem', city: 1 },
-  Ivan: { _id: 4, name: 'Ivan', city: -4 },
-  Nick: { _id: 5, name: 'Nick', city: 4 },
-  Mary: { _id: 6, name: 'Mary', city: 2 },
-  hol: { _id: 6.5, name: 'hol', city: 1 },
-  kol: { _id: 6.7, name: 'kol', city: 3 },
-  Ann: { _id: 7, name: 'Ann', city: 6 },
-  Kate: { _id: 8, name: 'Kate', city: 5 },
-  Kolya: { _id: 9, name: 'Kolya', city: 7 },
-  Dima: { _id: 10, name: 'Dima', city: -1 },
-};
+// const h = tree.getHeight();
+// console.log(h);
 
-for (const name in users) {
-  tree.add(users[name]);
-}
+// const b = tree.balanceOf(node40);
+// console.log(b);
 
-// const treeMin = tree.findMin();
-// const treeMax = tree.findMax();
+// const isB = tree.isBalanced();
+// console.log(isB);
 
-tree.remove(users.Nick);
-console.log(tree.root.right.right);
-
-// const treeMin = tree.findMin();
-// const treeMax = tree.findMax();
-
-// console.dir({ treeMin });
-// console.dir({ treeMax });
+tree.add({ num: 40 });
+tree.add({ num: 20 });
+tree.add({ num: 10 });
+tree.add({ num: 25 });
+tree.add({ num: 30 });
+// tree.add({ num: 22 });
+// tree.add({ num: 50 });
 
 // console.dir({ tree }, { depth: null });
-
-// const foundByKey = tree.findByKey( 'Kyiv' );
-
-// console.dir({foundByKey});
