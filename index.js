152e8c2e1ae14d6d69e15c04036ba292ab144547
Yevgen <yyakovliev02@gmail.com>
2021-05-28 14:35:25 +0300

    improved bst and store

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const DataStore = require('./lib/store.js');
 const db = new DataStore();
 
 db.index('age');
-db.index('city');
 db.index('name');
+db.index('city');
 
 const luke = db.add({ name: 'Luke', age: 17, city: 'Amsterdam' });
 const bob = db.add({ name: 'Bob', age: 18, city: 'Paris' });
 const joe = db.add({ name: 'Joe', age: 19, city: 'Warsaw' });
 const mary = db.add({ name: 'Mary', age: 20, city: 'Warsaw' });
 const dan = db.add({ name: 'Dan', age: 20, city: 'London' });
+const asdf = db.add({ name: 'asdf', age: 13, city: 'Amsterdam' });
 
-const greaterThanEqualTo18 = db.find({ age: { $gte: 18 } });
-const greaterThan18 = db.find({ age: { $gt: 18 } });
-const warsaw = db.find({ city: 'Warsaw' });
+const lessThan18 = db.find({ age: { $lt: 18 } });
+const greaterThan18 = db.find({ age: { $gte: 18 } });
+const amsterdam = db.find({ city: 'Amsterdam' });
 
-console.dir({ greaterThanEqualTo18 }, { depth: null });
-console.dir({ greaterThan18 }, { depth: null });
-console.dir({ warsaw }, { depth: null });
+console.log(lessThan18);
+console.log(greaterThan18);
+console.log(amsterdam);
