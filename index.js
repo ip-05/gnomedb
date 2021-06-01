34969179f38f91a1baacd0a65dde88fec555c5f7
Artem <bondarchuk.artem20@gmail.com>
2021-06-01 19:14:04 +0300

    prettier fixes

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 'use strict';
 
-const Store =  require('./lib/store.js');
+const Store = require('./lib/store.js');
 
-const db = new Store();
+const db = new Store().index('age');
+
+db.add({ age: 18 });
+db.add({ age: 22 });
+db.add({ age: 11 });
+db.add({ age: 20 });
+db.add({ age: 12 });
+db.add({ age: 19 });
+
+const res = db.find({ age: { $gt: 15, $lt: 20 } });
+
+console.log(res);
 
 /*
  *
@@ -11,4 +22,4 @@ const db = new Store();
  *
  */
 
-console.dir({ db }, { depth: null });
+//console.dir({ db }, { depth: null });
--- types/bst.d.ts
+++ types/bst.d.ts
@@ -1,16 +1,16 @@
 export = BinarySearchTree;
 declare class BinarySearchTree {
-    static compareKeys(a: any, b: any): any;
-    constructor(options?: {});
-    root: Node;
-    fieldKey: any;
-    add(data: any): any;
-    addFrom(obj: any): BinarySearchTree;
-    remove(data: any): void;
-    getHeight(node: any): any;
-    findByKey(key: any): any;
-    getNodesLessThan(val: any): Cursor;
-    getNodesGreaterThan(val: any): Cursor;
+  static compareKeys(a: any, b: any): any;
+  constructor(options?: {});
+  root: Node;
+  fieldKey: any;
+  add(data: any): any;
+  addFrom(obj: any): BinarySearchTree;
+  remove(data: any): void;
+  getHeight(node: any): any;
+  findByKey(key: any): any;
+  getNodesLessThan(val: any): Cursor;
+  getNodesGreaterThan(val: any): Cursor;
 }
-import Node = require("./node.js");
-import Cursor = require("./cursor.js");
+import Node = require('./node.js');
+import Cursor = require('./cursor.js');
