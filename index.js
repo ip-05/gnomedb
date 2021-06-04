044ae422409fd52304118b6f9af365fe74e1ccfd
Artem <bondarchuk.artem20@gmail.com>
2021-06-04 22:08:05 +0300

    changed index

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 'use strict';
 
-const Store = require('./lib/store.js');
+const DataStore = require('./lib/store.js');
 
-const db = new Store().index('age');
-
-db.add({ age: 18 });
-db.add({ age: 22 });
-db.add({ age: 11 });
-db.add({ age: 20 });
-db.add({ age: 12 });
-db.add({ age: 19 });
-
-const res = db.find({ age: { $gt: 15, $lt: 20 } });
-
-console.log(res);
-
-/*
- *
- * THIS FILE IS FOR TESTING YOUR CHANGES
- * THIS SHOULD NOT BE COMMITED!!!
- *
- */
-
-//console.dir({ db }, { depth: null });
+module.exports = DataStore;
