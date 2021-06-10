2e4df57628d3c8fa8508b81bb839e686fea96400
Yevgen <yyakovliev02@gmail.com>
2021-06-10 12:56:13 +0300

    added new types

--- types/bst.d.ts
+++ types/bst.d.ts
@@ -1,16 +1,16 @@
 export = BinarySearchTree;
 declare class BinarySearchTree {
-  static compareKeys(a: any, b: any): any;
-  constructor(options?: {});
-  root: Node;
-  fieldKey: any;
-  add(data: any): any;
-  addFrom(obj: any): BinarySearchTree;
-  remove(data: any): void;
-  getHeight(node: any): any;
-  findByKey(key: any): any;
-  getNodesLessThan(val: any): Cursor;
-  getNodesGreaterThan(val: any): Cursor;
+    static compareKeys(a: any, b: any): any;
+    constructor(options?: {});
+    root: Node;
+    fieldKey: any;
+    add(data: any): any;
+    addFrom(obj: any): BinarySearchTree;
+    remove(data: any): void;
+    getHeight(node: any): any;
+    findByKey(key: any): Cursor;
+    filterLessThan(val: any, equal?: boolean): Cursor;
+    filterGreaterThan(val: any, equal?: boolean): Cursor;
 }
-import Node = require('./node.js');
-import Cursor = require('./cursor.js');
+import Node = require("./node.js");
+import Cursor = require("./cursor.js");
